6634f7ff5b82097d043c6b68a9fbe95696441c91
Grzegorz Graczyk <gregory90@gmail.com>
2014-05-14 15:02:49 +0200

    Remove security dependency to prevent circular dependencies

--- Acl/Acl.php
+++ Acl/Acl.php
@@ -1,49 +1,18 @@
 <?php
 namespace PQstudio\RestUtilityBundle\Acl;
 
-use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
 use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
 use Symfony\Component\Security\Acl\Permission\MaskBuilder;
 
 
-/*
- * DI\Service("pq.acl")
- */
 class Acl
 {
     protected $acl;
 
-    protected $security;
-
-    /*
-     * DI\InjectParams({
-     *     "acl" = @DI\Inject("security.acl.provider"),
-     *     "security" = @DI\Inject("security.context")
-     * })
-     */
-    public function __construct($acl, $security)
+    public function __construct($acl)
     {
         $this->acl = $acl;
-        $this->security = $security;
-    }
-
-    public function addAclForCurrentUser($domainObject, $flags)
-    {
-        if($domainObject === null) {
-            return false;
-        }
-
-        $objectIdentity = ObjectIdentity::fromDomainObject($domainObject);
-        $acl = $this->acl->createAcl($objectIdentity);
-
-        $user = $this->security->getToken()->getUser();
-        $securityIdentity = UserSecurityIdentity::fromAccount($user);
-
-        $acl->insertObjectAce($securityIdentity, $flags);
-        $this->acl->updateAcl($acl);
-
-        return $this;
     }
 
     public function addAclForUser($domainObject, $user, $flags)
